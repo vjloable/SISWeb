20f83c16d877dcd9d5cbb715003972ff56df5f23
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-20 23:56:10 +0800

    refactor: get the cursor from the context manager for CollegeModel

--- app/models/college_model.py
+++ app/models/college_model.py
@@ -1,25 +1,28 @@
+from flask import g
+
 class CollegeModel:
     def __init__(self, code, name):
         self.code = code
         self.name = name
 
-    @classmethod
-    def insert(self, cursor):
+    def insert(self):
+        cursor = g.db_con.cursor()
         cursor.execute("""
         INSERT INTO Colleges
         VALUES ({}, {});
         """.format(self.code, self.name)
         )
     
-    @classmethod
-    def list_all(self, cursor):
+    def list_all(self):
+        cursor = g.db_con.cursor()
         response = cursor.execute("""
         SELECT * FROM Colleges;
         """)
         return response
 
     @staticmethod
-    def create_table(cursor):
+    def create_table():
+        cursor = g.db_con.cursor()
         cursor.execute("""
         CREATE TABLE IF NOT EXISTS Colleges( 
             Code char(20) NOT NULL,
