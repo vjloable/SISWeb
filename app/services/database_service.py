55c841ea37076a097a892d1fe2f88ff3e7c4b002
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-20 12:07:05 +0800

    feat: add a new method to create tables during initialization

--- app/services/database_service.py
+++ app/services/database_service.py
@@ -3,6 +3,9 @@ import os
 
 from flask import g
 from dotenv import load_dotenv
+from app.models.college_model import CollegeModel 
+from app.models.course_model import CourseModel 
+from app.models.student_model import StudentModel 
 
 load_dotenv()
 
@@ -13,21 +16,36 @@ class DatabaseService:
         self.host = os.getenv('MYSQL_HOST')
         self.database = os.getenv('MYSQL_DATABASE')
         self.connection = None
+
+        print("Initialize database")
     
     def __del__(self):
         self.close()
+        print("Deleting database instance")
 
     def connect(self):
         if self.connection is None:
-            self.connection = mysql.connector.connect({
-                "user": self.user,
-                "password": self.password,
-                "host": self.host,
-                "database": self.database
-                }) 
-        return self.connection
+            connection = mysql.connector.connect(
+                user = self.user,
+                password = self.password,
+                host = self.host,
+                database = self.database
+                )
+            cursor = connection.cursor()
+            self.create_tables(cursor)
+            print("Connecting to database")
+            self.connection = connection
+        return connection
+        
     
     def close(self):
+        print("Closing the database")
         if self.connection:
             self.connection.close()
             self.connection = None
+    
+    def create_tables(self, cursor):
+        print("Creating tables")
+        CollegeModel.create_table(cursor)
+        CourseModel.create_table(cursor)
+        StudentModel.create_table(cursor)
