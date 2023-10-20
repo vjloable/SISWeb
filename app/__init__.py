b79e3d86ce364fdbcef7118504863e3f92913e1b
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-21 03:15:19 +0800

    feat: add a method for loading schema

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,27 @@ import os
 from flask import Flask
 from app.controllers.college_controller import college_blueprint
 from app.controllers.index_controller import index_blueprint
+from app.models.college_model import CollegeModel 
+from app.models.course_model import CourseModel 
+from app.models.student_model import StudentModel
 
-def create_app():
+def create_app(connection):
     app = Flask(__name__)
-    
-    app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-    app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
-    app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DATABASE')
+    load_schema(connection)
+    with app.app_context():
+        app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+        app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+        app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+        app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DATABASE')
+        app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
-    app.register_blueprint(index_blueprint)
-    app.register_blueprint(college_blueprint)
+        app.register_blueprint(index_blueprint)
+        app.register_blueprint(college_blueprint)
 
-    return app
+        return app
+
+def load_schema(connection):
+    cursor = connection.cursor()    
+    CollegeModel.create_table(cursor)
+    CourseModel.create_table(cursor)
+    StudentModel.create_table(cursor)
