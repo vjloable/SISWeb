9f2b639f4d2faf999c8f6ab88ee59d1e626603f2
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-21 03:18:00 +0800

    fix: add error handling to insert and list_all methods for the CollegeModel

--- app/models/college_model.py
+++ app/models/college_model.py
@@ -1,30 +1,48 @@
 from flask import g
+from app.services.database_service import DatabaseService
+from mysql.connector import Error as MySQLError
 
 class CollegeModel:
-    def __init__(self, code, name):
-        self.code = code
-        self.name = name
+    def insert(self, code, name):
+        connection = DatabaseService().connect()
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+            INSERT INTO Colleges
+            VALUES ("{}", "{}");
+            """.format(code, name)
+            )
+            response = cursor.fetchall()
+            return {'success':'true', "response":str(response)}
+        except MySQLError as e:
+            return {'success':'false', 'response':str(e)}
+        finally:
+            cursor.close()
+            connection.close()
 
-    def insert(self):
-        cursor = g.db_con.cursor()
-        cursor.execute("""
-        INSERT INTO Colleges
-        VALUES ({}, {});
-        """.format(self.code, self.name)
-        )
-    
     def list_all(self):
-        cursor = g.db_con.cursor()
-        response = cursor.execute("""
-        SELECT * FROM Colleges;
-        """)
-        return response
+        connection = DatabaseService().connect()
+        cursor = connection.cursor()
+        try:
+            cursor.execute("""
+            SELECT * FROM Colleges;
+            """)
+            response = []
+            row = cursor.fetchall()
+            for data in row:
+                response.append({"code":str(data[0]), "name":str(data[1])})
+            return {'success':'true', "response":response}
+        except MySQLError as e:
+            return {'success':'false', 'response':str(e)}
+        finally:
+            cursor.close()
+            connection.close()
 
     @staticmethod
-    def create_table():
-        cursor = g.db_con.cursor()
+    def create_table(connection):
+        cursor = connection.cursor()
         cursor.execute("""
-        CREATE TABLE IF NOT EXISTS Colleges( 
+        CREATE TABLE IF NOT EXISTS Colleges(
             Code char(20) NOT NULL,
             Name char(150) NOT NULL,
             PRIMARY KEY (Code)
