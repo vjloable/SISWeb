4bc9a88bc133721cc874b4d045e9a29522544af9
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-20 21:41:08 +0800

    fix: change the decorator to index_blueprint

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -2,7 +2,7 @@ from flask import render_template, Blueprint
 
 index_blueprint = Blueprint('index', __name__)
 
-@test_blueprint.route('/')
-@test_blueprint.route('/index')
+@index_blueprint.route('/')
+@index_blueprint.route('/index')
 def index():
     return render_template('index.html')
