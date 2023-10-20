efb0715d5617a274f7688d84f225eae72058c05e
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-20 21:40:04 +0800

    refactor: rename import of test_controller and test_blueprint to index_controller and index_blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask
 from app.controllers.college_controller import college_blueprint
-from app.controllers.test_controller import test_blueprint
+from app.controllers.index_controller import index_blueprint
 
 def create_app():
     app = Flask(__name__)
@@ -12,7 +12,7 @@ def create_app():
     app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
     app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DATABASE')
 
-    app.register_blueprint(test_blueprint)
+    app.register_blueprint(index_blueprint)
     app.register_blueprint(college_blueprint)
 
     return app
