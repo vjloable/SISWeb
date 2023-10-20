872b9da9363b85427227ab733c640dc5ea2f16b8
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-20 12:05:42 +0800

    feat: initialize the database service and connection

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from app import create_app
+from app.services.database_service import DatabaseService
 from dotenv import load_dotenv
 
 load_dotenv()
 app = create_app()
+database_service = DatabaseService()
+database_connection = database_service.connect()
+
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    database_connection.close()
+    database_service.close()
 
 if __name__ == "__main__":
     app.run()
