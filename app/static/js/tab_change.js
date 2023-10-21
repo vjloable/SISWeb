e9c19172f8104b60cdae472bc88d89e6e9404e90
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-22 03:05:46 +0800

    feat: add a display that triggers reading an API for tabColleges

--- app/static/js/tab_change.js
+++ app/static/js/tab_change.js
@@ -1,3 +1,17 @@
 $('.menu .item')
-    .tab()
-;
+    .tab({
+      onVisible: function() {
+        var activeTab = $('.tab.segment.active');
+        var activeTabName = activeTab.attr('data-tab');
+        $.ajax({
+            type: 'GET',
+            url: '/api/'+activeTabName,
+            success: function (response) {
+                $('#'+activeTabName).html(response.content);
+            },
+            error: function (error) {
+                console.log('Error:', error);
+            }
+        });
+      }
+    });
