6b8d1d11b02835321b864b0e807a8eaaf6a3bca7
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-22 04:21:10 +0800

    feat: add a loading indicator when fetching the data

--- app/static/js/tab_change.js
+++ app/static/js/tab_change.js
@@ -4,14 +4,17 @@ $('.menu .item')
         var activeTab = $('.tab.segment.active');
         var activeTabName = activeTab.attr('data-tab');
         $.ajax({
-            type: 'GET',
-            url: '/api/'+activeTabName,
-            success: function (response) {
-                $('#'+activeTabName).html(response.content);
-            },
-            error: function (error) {
-                console.log('Error:', error);
-            }
+          type: 'GET',
+          url: '/api/'+activeTabName,
+          beforeSend: function() {
+            $('#'+activeTabName).html('<div class="ui indeterminate text loader">Fetching data</div>');
+          },
+          success: function (response) {
+            $('#'+activeTabName).html(response.content);
+          },
+          error: function (error) {
+            console.log('Error:', error);
+          }
         });
       }
     });
