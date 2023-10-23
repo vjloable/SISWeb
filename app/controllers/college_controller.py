f70b4ff16e2ea1e76b7a70cb6dfb6f8ea281fe7c
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-23 15:33:40 +0800

    refactor: transfer jsonify calls to CollegeView

--- app/controllers/college_controller.py
+++ app/controllers/college_controller.py
@@ -1,22 +1,20 @@
-from flask import Blueprint, render_template, jsonify, request, g
+from flask import Blueprint, request
 from app.models.college_model import CollegeModel
+from app.views.college_view import CollegeView
 
 college_blueprint = Blueprint('college', __name__)
 
 @college_blueprint.route('/api/tabColleges', methods=['GET'])
 def api_display_tab_college():
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
-        response = CollegeModel().count_rows()
+        response = CollegeModel.count_rows()
         if response['response'] > 0:
-            colleges = CollegeModel().list_all()
-            headers = ["Code", "College"]
-            data = {'content': render_template('content/table_view.html', headers=headers, rows=colleges['response'])}
-            return jsonify(data)
+            model_response = CollegeModel.list_all()
+            return CollegeView.renderTableAsJSON(**model_response)
         else:
-            data = {'content': render_template('content/warning_view.html', description="", button_text="")}
-            return jsonify(data)
+            return CollegeView.renderNoDataAsJSON()
     else:
-        return jsonify({'error': 'Unwanted request header'}), 400
+        return CollegeView.setPayloadToJSON(403)
 
 @college_blueprint.route('/api/college/create', methods=['POST'])
 def api_create_college():
@@ -24,15 +22,15 @@ def api_create_college():
     if request_body:
         code = str(request_body['code']) 
         name = str(request_body['name'])
-        colleges = CollegeModel().insert(code, name)
-        return jsonify(response = colleges), 201
+        model_response = CollegeModel.insert(code, name)
+        return CollegeView.setPayloadToJSON(201, payload=model_response)
     else:
-        return jsonify({'error': 'Invalid request data'}), 400
+        return CollegeView.setPayloadToJSON(400)
 
 @college_blueprint.route('/api/college/read/<code>', methods=['GET'])
 def api_read_college(code):
-    college = CollegeModel().read(code)
-    return jsonify(college), 201
+    model_response = CollegeModel.read(code)
+    return CollegeView.setPayloadToJSON(201, payload=model_response)
 
 @college_blueprint.route('/api/college/update', methods=['POST'])
 def api_update_college():
@@ -42,22 +40,22 @@ def api_update_college():
         name = str(request_body['name'])
         new_code = str(request_body['new_code'])
         new_name = str(request_body['new_name'])
-        college = CollegeModel().update(code, name, new_code, new_name)
-        return jsonify(response = college), 201
+        model_response = CollegeModel.update(code, name, new_code, new_name)
+        return CollegeView.setPayloadToJSON(201, payload=model_response)
     else:
-        return jsonify({'error': 'Invalid request data'}), 400
+        return CollegeView.setPayloadToJSON(400)
 
 @college_blueprint.route('/api/college/delete', methods=['POST'])
 def api_delete_college():
     request_body = request.get_json()
     if request_body:
         code = str(request_body['code'])
-        college = CollegeModel().delete(code)
-        return jsonify(response = college), 201
+        model_response = CollegeModel.delete(code)
+        return jsonify(**model_response), 201
     else:
-        return jsonify({'error': 'Invalid request data'}), 400
+        return CollegeView.setPayloadToJSON(400)
 
 @college_blueprint.route('/api/college/list', methods=['GET'])
 def api_get_colleges():
-    colleges = CollegeModel().list_all()
-    return jsonify(colleges), 201
+    model_response = CollegeModel.list_all()
+    return CollegeView.setPayloadToJSON(201, payload=model_response)
