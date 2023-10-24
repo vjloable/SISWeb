16c0ebc0c6ffa8df5845220c3051113f24b66660
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-24 19:33:42 +0800

    fix: fix the first tab not showing contents by calling it when the document is ready

--- app/static/js/tab_change.js
+++ app/static/js/tab_change.js
@@ -1,20 +1,37 @@
+$(document).ready(function () {
+  var activeTabName = 'tabStudents';
+  $.ajax({
+    type: 'GET',
+    url: '/api/'+activeTabName,
+    beforeSend: function() {
+      $('#'+activeTabName).html('<div class="ui indeterminate text loader">Fetching data</div>');
+    },
+    success: function (response) {
+      $('#'+activeTabName).html(response.content);
+    },
+    error: function (error) {
+      console.log('Error:', error);
+    }
+  });
+});
+
 $('.menu .item')
-    .tab({
-      onVisible: function() {
-        var activeTab = $('.tab.segment.active');
-        var activeTabName = activeTab.attr('data-tab');
-        $.ajax({
-          type: 'GET',
-          url: '/api/'+activeTabName,
-          beforeSend: function() {
-            $('#'+activeTabName).html('<div class="ui indeterminate text loader">Fetching data</div>');
-          },
-          success: function (response) {
-            $('#'+activeTabName).html(response.content);
-          },
-          error: function (error) {
-            console.log('Error:', error);
-          }
-        });
-      }
-    });
+  .tab({
+    onVisible: function() {
+      var activeTab = $('.tab.segment.active');
+      var activeTabName = activeTab.attr('data-tab');
+      $.ajax({
+        type: 'GET',
+        url: '/api/'+activeTabName,
+        beforeSend: function() {
+          $('#'+activeTabName).html('<div class="ui indeterminate text loader">Fetching data</div>');
+        },
+        success: function (response) {
+          $('#'+activeTabName).html(response.content);
+        },
+        error: function (error) {
+          console.log('Error:', error);
+        }
+      });
+    },
+  });
