d498fe9df09c5b18d2bf0c72e15a675220a85b7c
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-26 06:01:05 +0800

    feat: add a method for rendering the creation view for the college

--- app/views/college_view.py
+++ app/views/college_view.py
@@ -3,14 +3,21 @@ from flask import render_template, jsonify, make_response
 class CollegeView:
 
     @staticmethod
+    def renderCreateFormAsView():
+        rendered_view = render_template('tab_contents/form_template.html', form_content="College")
+        return rendered_view
+
+    @staticmethod
     def renderTableAsJSON(model_response):
         headers = ["Code", "College"]
-        data = {'content': render_template('content/table_template.html', headers=headers, rows=model_response['response'])}
+        content = render_template('tab_contents/table_template.html', headers=headers, rows=model_response['response'])
+        data = {'content': content}
         return jsonify(data)
     
     @staticmethod
     def renderNoDataAsJSON():
-        data = {'content': render_template('content/nodata_template.html', description="colleges", button_text="College")}
+        content = render_template('tab_contents/nodata_template.html', description="colleges", button_text="College")
+        data = {'content': content}
         return jsonify(data)
     
     @staticmethod
