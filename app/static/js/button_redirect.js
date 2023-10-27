38d961d45620e4822ee9c6561746a289f8360460
Eagl3ye <vincejaphethloable@gmail.com>
2023-10-28 01:12:45 +0800

    feat: add delete button scripts for college, course, and student

--- app/static/js/button_redirect.js
+++ app/static/js/button_redirect.js
@@ -10,6 +10,60 @@ $(document).on('click', '#gotoAddStudent', function() {
   window.location.href = '/student/create';
 });
 
+$(document).on('click', '#gotoDeleteCollege', function() {
+  var buttonValue = $(this).attr('value');
+  var data = {
+    'code': buttonValue
+  };
+  $.ajax({
+    type: 'POST',
+    url: "/api/college/delete",
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(response) {
+      window.location.reload();
+    },
+  });
+});
+
+$(document).on('click', '#gotoDeleteCourse', function() {
+  var buttonValue = $(this).attr('value');
+  var data = {
+    'code': buttonValue
+  };
+  $.ajax({
+    type: 'POST',
+    url: "/api/course/delete",
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(response) {
+      window.location.reload();
+    },
+  });
+});
+
+$(document).on('click', '#gotoDeleteStudent', function() {
+  var buttonValue = $(this).attr('value');
+  var data = {
+    'student_id': buttonValue
+  };
+  $.ajax({
+    type: 'POST',
+    url: "/api/student/delete",
+    type: "POST",
+    data: JSON.stringify(data),
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(response) {
+      window.location.reload();
+    },
+  });
+});
+
 $(document).on('click', '.cancel-button', function() {
   window.location.href = '/';
 });
