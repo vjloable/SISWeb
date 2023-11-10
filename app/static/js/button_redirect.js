3943c61409b992574c92205bd0bf1ef4f1b93aa2
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-11-10 14:56:15 +0800

    feat: add button click listener for functions of edit and delete in students, colleges, and courses.

--- app/static/js/button_redirect.js
+++ app/static/js/button_redirect.js
@@ -11,55 +11,141 @@ $(document).on('click', '#gotoAddStudent', function() {
 });
 
 $(document).on('click', '#gotoDeleteCollege', function() {
-  var buttonValue = $(this).attr('value');
-  var data = {
-    'code': buttonValue
-  };
-  $.ajax({
-    type: 'POST',
-    url: "/api/college/delete",
-    type: "POST",
-    data: JSON.stringify(data),
-    contentType:"application/json; charset=utf-8",
-    dataType:"json",
-    success: function(response) {
-      window.location.reload();
-    },
-  });
+   $('#deleteConfirmationModal')
+  .modal({
+    closable  : false,
+    onDeny    : function(){},
+    onApprove : function() {
+      var buttonValue = $(gotoDeleteCollege).attr('value');          
+      var data = {
+        'code': buttonValue
+      };
+      $.ajax({
+        type: 'POST',
+        url: "/api/college/delete",
+        data: JSON.stringify(data),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(response) {
+          window.location.reload();
+        },
+      });
+    }
+  })
+  .modal('show');
 });
 
 $(document).on('click', '#gotoDeleteCourse', function() {
-  var buttonValue = $(this).attr('value');
+  $('#deleteConfirmationModal')
+  .modal({
+    closable  : false,
+    onDeny    : function(){},
+    onApprove : function() {
+      var buttonValue = $(gotoDeleteCourse).attr('value');          
+      var data = {
+        'code': buttonValue
+      };
+      $.ajax({
+        type: 'POST',
+        url: "/api/course/delete",
+        data: JSON.stringify(data),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(response) {
+          window.location.reload();
+        },
+      });
+    }
+  })
+  .modal('show');
+});
+
+$(document).on('click', '#gotoDeleteStudent', function() {
+  $('#deleteConfirmationModal')
+  .modal({
+    closable  : false,
+    onDeny    : function(){},
+    onApprove : function() {
+      var buttonValue = $(gotoDeleteStudent).attr('value');
+      var data = {
+        'student_id': buttonValue
+      };
+      $.ajax({
+        type: 'POST',
+        url: "/api/student/delete",
+        data: JSON.stringify(data),
+        contentType:"application/json; charset=utf-8",
+        dataType:"json",
+        success: function(response) {
+          window.location.reload();
+        },
+      });
+    }
+  })
+  .modal('show');
+});
+
+$(document).on('click', '#gotoEditCollege', function() {
+  var buttonValue = $(gotoEditCollege).attr('value');
   var data = {
     'code': buttonValue
   };
   $.ajax({
-    type: 'POST',
-    url: "/api/course/delete",
-    type: "POST",
-    data: JSON.stringify(data),
+    type: 'GET',
+    url: "/api/college/read",
+    data: data,
     contentType:"application/json; charset=utf-8",
     dataType:"json",
     success: function(response) {
-      window.location.reload();
+      results = (response.results+"").split(",");
+      college_code = results[0];
+      college_name = results[1];
+      window.location.href = "/college/update?code="+college_code+"&name="+college_name;
     },
   });
 });
 
-$(document).on('click', '#gotoDeleteStudent', function() {
-  var buttonValue = $(this).attr('value');
+$(document).on('click', '#gotoEditCourse', function() {
+  var buttonValue = $(gotoEditCourse).attr('value');
+  var data = {
+    'code': buttonValue
+  };
+  $.ajax({
+    type: 'GET',
+    url: "/api/course/read",
+    data: data,
+    contentType:"application/json; charset=utf-8",
+    dataType:"json",
+    success: function(response) {
+      results = (response.results+"").split(",");
+      course_code = results[0];
+      course_name = results[1];
+      college_code = results[2];
+      window.location.href = "/course/update?code="+course_code+"&name="+course_name+"&college="+college_code;
+    },
+  });
+});
+
+$(document).on('click', '#gotoEditStudent', function() {
+  var buttonValue = $(gotoEditStudent).attr('value');
   var data = {
     'student_id': buttonValue
   };
   $.ajax({
-    type: 'POST',
-    url: "/api/student/delete",
-    type: "POST",
-    data: JSON.stringify(data),
+    type: 'GET',
+    url: "/api/student/read",
+    data: data,
     contentType:"application/json; charset=utf-8",
     dataType:"json",
     success: function(response) {
-      window.location.reload();
+      results = (response.results+"").split(",");
+      student_id = results[0]; 
+      firstname = results[1];
+      lastname = results[2];
+      course = results[3];
+      year = results[4];
+      gender = results[5];
+      window.location.href = "/student/update?student_id="+student_id+"&firstname="+firstname+"&lastname="+lastname+"&course="+course+"&year="+year+"&gender="+gender;
     },
   });
 });
