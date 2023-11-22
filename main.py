be935327bd5dfba38f633b03334dc8e566e9338f
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-11-23 02:48:21 +0800

    feat: add an initial configuration runner for the cloud service

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from app import create_app
 from dotenv import load_dotenv
 from app.services.database_service import DatabaseService
+from app.services.cloud_service import CloudService
 
 load_dotenv()
 
 database_service = DatabaseService()
 database_connection = database_service.connect()
+
+cloud_service = CloudService()
+cloud_service.configure()
+
 app = create_app(database_connection)
 
 @app.teardown_appcontext
