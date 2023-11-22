9447bfb026297d1ef93bf7be5429fb960075e497
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-11-23 02:49:28 +0800

    test: add an endpoint for cloud service upload testing

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,4 +1,6 @@
-from flask import render_template, Blueprint
+import os
+from flask import render_template, Blueprint, request
+from app.services.cloud_service import CloudService
 
 index_blueprint = Blueprint('index', __name__)
 
@@ -6,3 +8,16 @@ index_blueprint = Blueprint('index', __name__)
 @index_blueprint.route('/index')
 def index():
     return render_template('index.html')
+
+@index_blueprint.route('/test')
+def test():
+    return render_template('test.html')
+
+@index_blueprint.route('/upload', methods=['POST'])
+def upload():
+    if 'image' in request.files:
+        image = request.files['image']
+        url = CloudService.upload(image,"test","college")
+        return url
+    else:
+        return 'No image provided', 400
