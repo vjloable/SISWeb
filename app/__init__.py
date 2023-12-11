2f4eabfa57c79368c993c2603f20065a5fbb86c2
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-12-12 03:21:25 +0800

    feat: set the app secret and app config for flask session library

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 import os
 import cloudinary
 
-from flask import Flask
+from flask import Flask, session
 from app.controllers.index_controller import index_blueprint
 from app.controllers.college_controller import college_blueprint
 from app.controllers.course_controller import course_blueprint
@@ -12,6 +12,8 @@ from app.models.student_model import StudentModel
 
 def create_app(connection):
     app = Flask(__name__)
+    app.secret_key = os.getenv('APP_SECRET')
+
     load_schema(connection)
     load_cloudinary()
     with app.app_context():
@@ -20,6 +22,8 @@ def create_app(connection):
         app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
         app.config['MYSQL_DATABASE'] = os.getenv('MYSQL_DATABASE')
         app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+        app.config["SESSION_PERMANENT"] = False
+        app.config["SESSION_TYPE"] = "filesystem"
 
         app.register_blueprint(index_blueprint)
         app.register_blueprint(college_blueprint)
