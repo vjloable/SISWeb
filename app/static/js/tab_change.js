bfb147a0bd9c701622420b37674b9bdf3c39a825
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-12-12 03:26:43 +0800

    feat: add last tab management

--- app/static/js/tab_change.js
+++ app/static/js/tab_change.js
@@ -1,33 +1,40 @@
 $(document).ready(function () {
   $.ajax({
     type: 'GET',
-    url: '/api/college/list',
-    beforeSend: function() {
-      $('#collegeTab').html('<div class="ui indeterminate text loader">Fetching data</div>');
-    },
+    url: '/last_tab',
     success: function (results) {
-      $('#collegeTab').html(results.content);
-      $('#collegeTabControls').html(results.buttons);
+      return results;
     },
     error: function (error) {
       console.log('Error:', error);
     }
+  }).done((result) => {
+    activeTabName = result.toLowerCase();
+    console.log(result);
+    $.ajax({
+      type: 'GET',
+      url: '/api/' + activeTabName + '/list',
+      success: function (results) {
+        $('#' + activeTabName + 'Tab').html(results.content);
+        $('#' + activeTabName + 'TabControls').html(results.buttons);
+        $.tab('change tab', activeTabName);
+      },
+      error: function (error) {
+        console.log('Error:', error);
+      }
+    });
   });
-  
 });
 
 
 $('.menu .item').tab({
   onVisible: function() {
+    console.log("visible");
     var activeTab = $('.tab.segment.active');
     var activeTabName = activeTab.attr('data-tab');
-    
     $.ajax({
       type: 'GET',
       url: '/api/'+activeTabName+'/list',
-      beforeSend: function() {
-        $('#'+activeTabName+'Tab').html('<div class="ui indeterminate text loader">Fetching data</div>');
-      },
       success: function (results) {
         $('#'+activeTabName+'Tab').html(results.content);
         $('#'+activeTabName+'TabControls').html(results.buttons);
