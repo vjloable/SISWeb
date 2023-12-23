240c4eb2db6003520898a4f48b1c1430295fb9d9
Vince Loable <vincejapheth.loable@g.msuiit.edu.ph>
2023-12-23 13:28:45 +0800

    feat: add dropdown configurations when editing and adding

--- app/static/js/dropdowns.js
+++ app/static/js/dropdowns.js
@@ -2,7 +2,7 @@ $('.ui.dropdown').dropdown({
     clearable: true
 });
 
-$('#dropdownMenuCollege').dropdown({
+$('#dropdownMenuAddCollege').dropdown({
     clearable: true,
     apiSettings: {
         url: '/api/college/list',
@@ -35,7 +35,42 @@ $('#dropdownMenuCollege').dropdown({
     },
 });
 
-$('#dropdownMenuCourse').dropdown({
+$('#dropdownMenuEditCollege').dropdown({
+    clearable: true,
+    apiSettings: {
+        url: '/api/college/list',
+        method: 'GET',
+        cache: false,
+        saveRemoteData: false,
+        beforeSend: function(settings) {
+            const query = $('#dropdownMenuCollege input.search').val().trim();
+            settings.data = {query: query, page: -1};
+            settings.contentType = 'application/json';
+            return settings;
+        },
+        onResponse: function(response) {
+            results = response.results
+            var raw_response = []
+            for (let i = 0; i < (results).length; i++) {
+                raw_response.push({
+                    "name": (results)[i][1],
+                    "value": (results)[i][0]
+                });
+            }
+            return { success: response.success, results: raw_response};
+        },
+        onFailure: function (response) {
+            console.log("fail");
+        },
+        onError: function (response) {
+            console.log("error");
+        },
+    },
+})
+.dropdown('set value', new URL(window.location.href).searchParams.get("college"))
+.dropdown('set text', new URL(window.location.href).searchParams.get("college"));
+
+$('#dropdownMenuAddCourse').dropdown({
     clearable: true,
     apiSettings: {
         url: '/api/course/list',
@@ -67,3 +102,38 @@ $('#dropdownMenuCourse').dropdown({
         },
     },
 });
+
+$('#dropdownMenuEditCourse').dropdown({
+    clearable: true,
+    apiSettings: {
+        url: '/api/course/list',
+        method: 'GET',
+        cache: false,
+        saveRemoteData: false,
+        beforeSend: function(settings) {
+            const query = $('#dropdownMenuCourse input.search').val().trim();
+            settings.data = { query: query, page: -1 };
+            settings.contentType = 'application/json';
+            return settings;
+        },
+        onResponse: function(response) {
+            results = response.results
+            var raw_response = []
+            for (let i = 0; i < (results).length; i++) {
+                raw_response.push({
+                    "name": (results)[i][1],
+                    "value": (results)[i][0]
+                });
+            }
+            return { success: response.success, results: raw_response};
+        },
+        onFailure: function (response) {
+            console.log("fail");
+        },
+        onError: function (response) {
+            console.log("error");
+        },
+    },
+})
+.dropdown('set value', new URL(window.location.href).searchParams.get("course"))
+.dropdown('set text', new URL(window.location.href).searchParams.get("course"));
